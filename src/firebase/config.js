bb9367c1db44b95374f580ede2352096da449476
Samethic <oyebodes19@gmail.com>
2026-07-06 13:23:35 +0100

    Move Firebase config to environment variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAAha9U1mOQURp2TsXULSaMb4beMQVKznI",
-  authDomain: "skillmint-171fe.firebaseapp.com",
-  projectId: "skillmint-171fe",
-  storageBucket: "skillmint-171fe.firebasestorage.app",
-  messagingSenderId: "1017445275400",
-  appId: "1:1017445275400:web:6bf81c2206eb53f5523ebd"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
